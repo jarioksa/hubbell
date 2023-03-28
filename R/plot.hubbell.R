56248c894c0d885b5389da19bedfa61834851982
Jari Oksanen <jhoksane@gmail.com>
2023-03-28 14:49:31 +0300

    version 1.2 of 14/11/2002

--- R/plot.hubbell.R
+++ R/plot.hubbell.R
@@ -1,17 +1,19 @@
 "plot.hubbell" <-
-function (x, sp.max=64, ...) 
+function (x, sp.max = 64, ...) 
 {
-  op <- par(no.readonly=TRUE)
-  palette(rainbow(sp.max))
-  layout(matrix(c(1,1,2,1,1,2),2,3, byrow=TRUE))
-  dim <- ceiling(sqrt(length(x)))
-  xy <- expand.grid(1:dim, 1:dim)[1:length(x), ]
-  plot(xy, xlab="",ylab="",axes=F, pch=16, col=x, ...)
-  box()
-  rad <- rev(sort(table(x)))
-  plot(1:length(rad), rad, log="y", type="l", xlab="Rank", ylab="Abundance", 
-       xlim=c(1,sp.max), ylim=c(1,length(comm)), ...)
-  points(1:length(rad), rad, col=x, ...)
-  par(op)
-  invisible()
+    op <- par("mfrow")
+    palette(rainbow(sp.max))
+    layout(matrix(c(1, 1, 2, 1, 1, 2), 2, 3, byrow = TRUE))
+    dim <- ceiling(sqrt(length(x)))
+    xy <- expand.grid(1:dim, 1:dim)[1:length(x), ]
+    plot(xy, xlab = "", ylab = "", axes = FALSE, pch = 16, col = x, 
+         ...)
+    box()
+    rad <- rev(sort(table(x)))
+    plot(1:length(rad), rad, log = "y", type = "l", xlab = "Rank", 
+         ylab = "Abundance", xlim = c(1, sp.max),
+         ylim = c(1, length(x)), ...)
+    points(1:length(rad), rad, col = names(rad), pch=16, ...)
+    par(mfrow=op)
+    invisible()
 }
